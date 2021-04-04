e3e062506a5e56abf850c34d6f2730c9b5b959de
hika0403 <egirlshikaru_0221@icloud.com>
2021-04-04 17:42:06 +0900

    目標の削除実装

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,4 +1,6 @@
 class GoalsController < ApplicationController
+  before_action :set_goal, only: %i[ show edit update destroy ]
+
   def index
   end
 
@@ -19,7 +21,16 @@ class GoalsController < ApplicationController
   def show
   end
 
+  def destroy
+    @goal.destroy
+    redirect_to root_path
+  end
+
   private
+  def set_goal
+    @goal = Goal.find(params[:id])
+  end
+
   def goal_params
     params.require(:goal).permit(:weight_goal, :saving_goal, :purpose, :period).merge(user_id: current_user.id)
   end
