f70e35d9631db1eca7a54cc247522f3fde557e9d
hika0403 <egirlshikaru_0221@icloud.com>
2021-04-14 15:20:45 +0900

    体重記録詳細ページの実装

---
+++ app/controllers/bodies_controller.rb
@@ -0,0 +1,16 @@
+class BodiesController < ApplicationController
+
+  def show
+    @body = Body.all
+  end
+
+  def destroy
+    @body = Body.find(params[:id])
+    @body.destroy
+    respond_to do |format|
+      format.html { redirect_to money_index_url, notice: "Body was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "money#index"
   resources :money
-  resources :bodies, only: [:show]
+  resources :bodies, only: [:show, :destroy]
   resources :goals, only: [:new, :create, :show, :destroy]
 end
