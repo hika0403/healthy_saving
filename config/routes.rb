543e71f338292387210a8376d0722f96a6186c12
hika0403 <egirlshikaru_0221@icloud.com>
2021-04-18 12:59:08 +0900

    deviseコントローラーの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+  }
   root "money#index"
   resources :money
   resources :bodies, only: [:show, :destroy]
