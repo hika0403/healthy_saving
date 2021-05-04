1d8846ff31fef9e33fd54abe75b47792cebf5a12
hika0403 <egirlshikaru_0221@icloud.com>
2021-05-04 09:56:40 +0900

    トップページへ枠追加

--- app/models/body.rb
+++ app/models/body.rb
@@ -1,3 +1,8 @@
 class Body < ApplicationRecord
   belongs_to :user
+
+  def self.recentweight
+    allweight = Body.pluck(:weight)
+    allweight.last
+  end
 end
--- app/models/money.rb
+++ app/models/money.rb
@@ -1,3 +1,7 @@
 class Money < ApplicationRecord
   belongs_to :user
+
+  def self.sumprice
+    sum(:saving_amount)
+  end
 end
