6fe076b5c96146ddcd27a70b20544e18230a7225
hika0403 <egirlshikaru_0221@icloud.com>
2021-05-19 18:20:22 +0900

    バリデーションの設定

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,15 @@
 class Goal < ApplicationRecord
+  with_options presence: true do
+    validates :weight_goal
+    validates :saving_goal
+    validates :purpose
+    validates :period
+  end
+
+  with_options numericality: true do
+    validates :weight_goal
+    validates :saving_goal
+  end
+
   belongs_to :user
 end
