870d7ce2af52066a998cd04dd82d2c675c3ff387
hika0403 <egirlshikaru_0221@icloud.com>
2021-05-19 18:20:44 +0900

    テストコード実装

--- spec/factories/goals.rb
+++ spec/factories/goals.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
   factory :goal do
+    weight_goal { '50' }
+    saving_goal { '500000' }
+    purpose     { 'こんにちは' }
+    period      { '2040-01-02' }
+
+    association :user
   end
 end
--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe Goal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @goal = FactoryBot.build(:goal)
+  end
+
+  describe 'ゴールを設定する' do
+    context '設定できるとき' do
+      it '全ての入力があれば登録できること' do
+        expect(@goal).to be_valid
+      end
+      it 'weight_goalが半角数字であれば登録できる' do
+        @goal.weight_goal = 78987
+        @goal.save
+        expect(@goal).to be_valid
+      end
+      it 'saving_goalが半角数字であれば登録できる' do
+        @goal.saving_goal = 23456789
+        @goal.save
+        expect(@goal).to be_valid
+      end
+    end
+    context '設定できないとき' do
+      it 'weight_goalが空ではできない' do
+        @goal.weight_goal = ''
+        @goal.valid?
+        expect(@goal.errors.full_messages).to include "Weight goal can't be blank"
+      end
+      it 'weight_goalが半角数字でないとできない' do
+        @goal.weight_goal = '123abc'
+        @goal.valid?
+        expect(@goal.errors.full_messages).to include "Weight goal is not a number"
+      end
+      it 'saving_goalが空ではできない' do
+        @goal.saving_goal = ''
+        @goal.valid?
+        expect(@goal.errors.full_messages).to include "Saving goal can't be blank"
+      end
+      it 'saving_goalが半角数字でないとできない' do
+        @goal.saving_goal = '123abc'
+        @goal.valid?
+        expect(@goal.errors.full_messages).to include "Saving goal is not a number"
+      end
+      it 'purposeが空ではできない' do
+        @goal.purpose = ''
+        @goal.valid?
+        expect(@goal.errors.full_messages).to include "Purpose can't be blank"
+      end
+      it 'periodが空ではできない' do
+        @goal.period = ''
+        @goal.valid?
+        expect(@goal.errors.full_messages).to include "Period can't be blank"
+      end
+    end
+  end
 end
