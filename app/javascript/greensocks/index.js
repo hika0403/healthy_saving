0ed3b47860d88bdb429ae77e2b815d54eafa04cd
hika0403 <egirlshikaru_0221@icloud.com>
2021-05-24 16:17:46 +0900

    scrolltrigger実装

---
+++ app/javascript/greensocks/index.js
@@ -0,0 +1,18 @@
+// import { gsap } from "gsap";
+// import { ScrollTrigger } from 'gsap/ScrollTrigger'
+// gsap.registerPlugin(ScrollTrigger);
+
+// function pullDown() {
+
+//   const pullDownText = document.getElementsByClassName("welcome-text")
+//   const pullDownParents = document.getElementsByClassName("goal-image")
+
+//   gsap.from(".welcome-pulldown", {
+//     scrollTrigger: {
+//       trigger: "",
+//       start: "top center",
+//       markers: true
+//     }
+//   })
+// }
+//   window.addEventListener('load', pullDown)
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 import "chartkick/chart.js"
+require("greensocks")
+require('greensocks/index')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
