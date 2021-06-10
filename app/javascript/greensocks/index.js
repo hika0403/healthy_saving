53b02becf5ef68259daa83577a73535340e900f4
hika0403 <egirlshikaru_0221@icloud.com>
2021-06-10 19:45:44 +0900

    animation.cssの実装

--- app/javascript/greensocks/index.js
+++ app/javascript/greensocks/index.js
@@ -1,7 +1,17 @@
-// $(function () {
-//   $(document).on("mouseover", ".row-title", function () {
-//     $(this).css({ "color": "blue" });
-//   }).on("mouseout", ".row-title", function () {
-//     $(this).css({ "color": "" });
-//   })
-// });
+$(window).on('scroll', function (){
+
+  var anime = $('.animate__animated');
+  var isAnimate = 'animate__zoomIn';
+  
+  anime.each(function () {
+  
+    var elemOffset = $(this).offset().top;
+    var scrollPos = $(window).scrollTop();
+    var wh = $(window).height();
+
+    if(scrollPos > elemOffset - wh + (wh / 3) ){
+      $(this).addClass(isAnimate);
+    }
+  });
+
+});
